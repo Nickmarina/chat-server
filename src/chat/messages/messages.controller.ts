f8af226e513ff5eb0861cca9c87a05ee1f72443d
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2021-10-10 23:50:17 +0300

    Fixed messages get request

--- src/chat/messages/messages.controller.ts
+++ src/chat/messages/messages.controller.ts
@@ -1,28 +1,26 @@
-import { Controller, Body, Param, Get, Post } from '@nestjs/common';
+import { Controller, Body, Param, Query, Get, Post } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from '../dto/create-message.dto';
 import { Message } from '../schemas/message.schema';
+// import { query } from 'express';
 
 @Controller('messages')
 export class MessagesController {
-    constructor(private readonly messagesService: MessagesService) {}
-
-//   @Get()
-//   getAll(@Body(sender_id:string, resipient_id:string): Promise<Message[]> {
-//     return this.messagesService.getAll({sender_id, resipient_id});
-//   }
+  constructor(private readonly messagesService: MessagesService) {}
 
   // @Get()
-  // get(@Param('id') id: string): Promise<Message[]> {
-  //   return this.messagesService.getMessages(id);
+  // getAll(): Promise<Message[]> {
+  //   return this.messagesService.getAll();
   // }
 
+  
   @Get()
-  getAll(): Promise<Message[]> {
-    return this.messagesService.getAll();
+  getAll(@Query() query): Promise<Message[]> {
+    return this.messagesService.getAll({query});
   }
+
   @Post()
   create(@Body() createMessageDto: CreateMessageDto): Promise<Message> {
-    return this.messagesService.create(createMessageDto)
+    return this.messagesService.create(createMessageDto);
   }
 }
--- src/chat/messages/messages.service.ts
+++ src/chat/messages/messages.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { async } from 'rxjs';
 import { CreateMessageDto } from '../dto/create-message.dto';
 import { Message, MessageDocument } from '../schemas/message.schema';
 
@@ -10,28 +11,21 @@ export class MessagesService {
     @InjectModel(Message.name) private messageModel: Model<MessageDocument>,
   ) {}
 
-  // async getAll(senderId: string, resipientId: string): Promise<Message[]> {
-  //   return this.messageModel.find(senderId && resipientId);
-  // }
-  async getAll(): Promise<Message[]> {
+  async getAll({ query }): Promise<Message[]> {
+    console.log(query);
     const messages = await this.messageModel.find().exec();
-    // const newList = messages.filter((message) => message);
-    // messages.map((message) => {
-    //   const userMessages = [];
-    //   const values = Object.values(message);
-    //   for (const value of values){
-    //     if (value.includes(id)){
-    //       userMessages.push(message);
-    //     }
-    //   }
-    //   return userMessages;
-    //   // if (message.sender_id === id || message.recipient_id === id) {
-    //   //   const userMessages = [];
-    //   //   userMessages.push(message);
-    //   //   return userMessages;
-    //   // }
-    // });
-    return messages;
+    const newMessages = messages.filter((message) => {
+      const values = Object.values(query);
+        for (const value of values) {
+          if (
+            JSON.stringify(message.sender_id) === JSON.stringify(value) ||
+            JSON.stringify(message.recipient_id) === JSON.stringify(value)
+          ) {
+            return message;
+          }
+        }
+    })
+    return newMessages;
   }
 
   async create(messageDto: CreateMessageDto): Promise<Message> {
